66459855f96eb535200519e4fe25d7b8545e298c
yan2xme <39150565+yan2xme@users.noreply.github.com>
2025-05-09 02:45:40 +0800

    Implemented photo gallery option for the blog (JQuery) & add nested write_a_comment

--- main.js
+++ main.js
@@ -23,3 +23,13 @@ appearOptions);
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
+
+
+$(".images img").click(function(){
+    $("#full-image").attr("src", $(this).attr("src"));
+    $('#image-viewer').show();
+});
+
+$("#image-viewer .close").click(function(){
+    $('#image-viewer').hide();
+});
